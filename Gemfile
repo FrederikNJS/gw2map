44119bbf9f186634691ce6a47ac5b66f19928e71
Frederik Nordahl Jul Sabroe <frederik.sabroe@bestbrains.dk>
2014-07-28 19:54:38 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+ruby '2.1.2'
+
+gem 'sinatra', '1.4.4'
+gem 'thin', '1.6.2'
+gem 'rake', '10.3.1'
+
+group :test do
+  gem 'rspec', '2.14.1'
+  gem 'rack-test', '0.6.2'
+  gem 'capybara', '2.2.1'
+  gem 'poltergeist', '1.5.0'
+end
---
+++ public/javascript/require.config.js
@@ -0,0 +1,41 @@
+requirejs.config({
+    paths: {
+        leaflet: "/bower_components/leaflet-0.7.3/leaflet",
+        jquery: "/bower_components/jquery/dist/jquery.min",
+        q: "/bower_components/q/q"
+    }
+});
+
+require(['leaflet', 'jquery'], function(leaflet, $) {
+    "use strict";
+    var attribution = "© 2014 ArenaNet, LLC. All rights reserved. NCSOFT, the interlocking NC logo, ArenaNet, Guild Wars, Guild Wars Factions, Guild Wars Nightfall, Guild Wars: Eye of the North, Guild Wars 2, and all associated logos and designs are trademarks or registered trademarks of NCSOFT Corporation. All other trademarks are the property of their respective owners.";
+    var continents = $.getJSON("https://api.guildwars2.com/v1/continents.json");
+    var floor = $.getJSON("https://api.guildwars2.com/v1/map_floor.json?continent_id=1&floor=1");
+
+    continents.done(function(continents) {
+        var selectedContinent = 1;
+        var startFloor = 1;
+        //continents.continents[selectedContinent].floors
+        var layers = {};
+        [1,2,3].forEach(function(floor_id) {
+            layers[floor_id] = leaflet.tileLayer('https://tiles{s}.guildwars2.com/{continent}/{floor}/{z}/{x}/{y}.jpg', {
+                attribution: attribution,
+                minZoom: 2,
+                maxZoom: 7,
+                continuousWorld: true,
+                subdomains: [1, 2, 3, 4 ],
+                floor: floor_id,
+                continent: selectedContinent
+            });
+        });
+
+        var map = leaflet.map('map', {
+            crs: leaflet.CRS.Simple,
+            layers: [layers[startFloor]]
+        });
+        leaflet.control.layers(layers).addTo(map);
+        var boundaries = new leaflet.LatLngBounds(map.unproject([0, 32768], map.getMaxZoom()), map.unproject([32768, 0], map.getMaxZoom()));
+        map.setView(map.unproject([32768/2, 32768/2], map.getMaxZoom()), 3);
+        map.setMaxBounds(boundaries);
+    });
+});
