917962a162b0a5b20698e8b958497d4392ed4fe6
Frederik Nordahl Jul Sabroe <fs@blackwoodseven.com>
2016-01-02 22:36:48 +0100

    Added Continent domain class

---
+++ javascript/continent.js
@@ -0,0 +1,12 @@
+import Floor from 'javascript/floor'
+
+export default class Continent {
+    constructor(continentDef) {
+        this.id = continentDef.get('id')
+        this.name = continentDef.get('name')
+        this.dims = continentDef.get('continent_dims')
+        this.minZoom = continentDef.get('min_zoom')
+        this.maxZoom = continentDef.get('max_zoom')
+        this.floorIds = continentDef.get('floors')
+    }
+}
