426fda3b4363df8464f7df17933ba799a0ee3d32
Frederik Nordahl Jul Sabroe <fs@blackwoodseven.com>
2016-01-17 00:13:15 +0100

    Added tests for Rect.

---
+++ test/model/rect-spec.js
@@ -0,0 +1,22 @@
+import Immutable from 'immutable'
+import Rect from 'javascript/model/rect'
+
+describe("A Rect", function() {
+  const rawRect = Immutable.fromJS([[1, 2], [3, 4]])
+  const rect = new Rect(rawRect)
+
+  it('should instantiate correctly', function() {
+    expect(rect.topLeft.constructor.name).toEqual('Coordinate')
+    expect(rect.bottomRight.constructor.name).toEqual('Coordinate')
+
+    expect(rect.topLeft.x).toEqual(1)
+    expect(rect.topLeft.y).toEqual(2)
+    expect(rect.bottomRight.x).toEqual(3)
+    expect(rect.bottomRight.y).toEqual(4)
+  })
+
+  it('should be able to calculate it\'s center', function() {
+    expect(rect.center.x).toEqual(2)
+    expect(rect.center.y).toEqual(3)
+  })
+})
